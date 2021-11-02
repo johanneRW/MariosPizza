fc275e1afb0f2051dcecfe994ec485c8ba7b117c
johanne <johanne_riis@hotmail.com>
2021-11-02 15:04:36 +0100

    Changes by Mikkel Sandell

--- src/company/Pizza.java
+++ src/company/Pizza.java
@@ -8,11 +8,32 @@ public class Pizza {
     private String[] ingredients;
     private int prize;
 
-    public Pizza(int number, String name, String ingredients,int prize){
-        this.number=number;
-        this.name =name;
-        this.ingredients=ingredients;
+    public Pizza(String name, String[] ingredients,int prize){
+
+        this.name = name;
+        this.ingredients = ingredients;
         this.prize = prize;
     }
 
+    @Override
+    public String toString() {
+        return name + "\nIngredienser: \n" + getIngredients() + "\n" + prize + " kr";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrize() {
+        return prize;
+    }
+
+    public String getIngredients() {
+        StringBuilder sb = new StringBuilder();
+        for (String s : this.ingredients){
+            sb.append(s);
+            sb.append(" ");
+        }
+        return sb.toString();
+    }
 }
