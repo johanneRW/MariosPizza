6991c762e10d1c7780fd68db0e2fa8229a3c99fb
johanne <johanne_riis@hotmail.com>
2021-11-02 11:09:13 +0100

    Pizza

---
+++ src/Pizza.java
@@ -0,0 +1,15 @@
+public class Pizza {
+
+    private String name;
+    private String ingredients;
+    private int number;
+    private int prize;
+
+    public Pizza(int number, String name, String ingredients,int prize){
+        this.number=number;
+        this.name =name;
+        this.ingredients=ingredients;
+        this.prize = prize;
+    }
+
+}
